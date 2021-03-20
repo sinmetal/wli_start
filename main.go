155031104ddc42fd2d4397adb9eb4c6ec3401663
sinmetal <sinmetal@sinmetalcraft.jp>
2021-03-20 19:58:44 +0900

    add sa token

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
 
 	"cloud.google.com/go/compute/metadata"
 )
@@ -13,9 +16,38 @@ func main() {
 
 	ctx := context.Background()
 
-	saEmail, err := metadata.Email("")
-	if err != nil {
-		log.Fatal(ctx, err.Error())
+	for {
+		time.Sleep(3 * time.Second)
+
+		token, err := getServiceAccountToken(ctx)
+		if err != nil {
+			fmt.Printf("failed getServiceAccountToken : %s\n", err)
+		}
+		fmt.Printf("SAToken : %s\n", token)
+
+		saEmail, err := metadata.Email("")
+		if err != nil {
+			log.Fatal(ctx, err.Error())
+		}
+		fmt.Printf("I am %s\n", saEmail)
 	}
-	fmt.Printf("I am %s\n", saEmail)
+
+}
+
+func getServiceAccountToken(ctx context.Context) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token", nil)
+	if err != nil {
+		return "", fmt.Errorf("failed getServiceAccountToken http.NewRequest : %w", err)
+	}
+	req.Header.Set("Metadata-Flavor", "Google")
+	req.WithContext(ctx)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed getServiceAccountToken http.Do : %w", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed getServiceAccountToken ioutil.ReadAll : %w", err)
+	}
+	return string(body), nil
 }
